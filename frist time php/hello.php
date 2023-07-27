ce1afcd444ea1692dfb94389c8a8fb50648e591c
afuzapratama <afuzapratama@yahoo.com>
2023-07-27 09:54:47 +0700

    frist time

---
+++ frist time php/hello.php
@@ -0,0 +1,7 @@
+<?php
+echo "hellow world </br>"; // echo is a statement
+
+
+echo 'hellow world </br>'; // echo is a statement
+
+printf("hellow world </br>"); // printf is a function
---
+++ frist-time-php/README.md
@@ -0,0 +1,3 @@
+Frist time for the learning php script
+
+i want to be able to create
