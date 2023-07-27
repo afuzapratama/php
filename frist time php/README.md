21f047f51657e40cbaeb1519126560358adb1919
afuzapratama <45757994+afuzapratama@users.noreply.github.com>
2023-07-27 09:58:44 +0700

    DELETE DUPLICATE DIRECTORY

--- frist time php/README.md
+++
@@ -1,3 +0,0 @@
-Frist time for the learning php script
-
-i want to be able to create
--- frist time php/hello.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "hellow world </br>"; // echo is a statement
-
-
-echo 'hellow world </br>'; // echo is a statement
-
-printf("hellow world </br>"); // printf is a function
